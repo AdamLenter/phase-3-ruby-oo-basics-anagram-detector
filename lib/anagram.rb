c0893c1f977cec2be9ce2b09e3757d514cbbdbde
Adam <adamlenter1@gmail.com>
2022-03-03 12:53:50 -0500

    commit tree a109fc87838d37f8371e8647e9069ef26afb74ee

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list_of_words)
+        matches = []
+        original_word = @word.upcase.chars.sort
+
+        list_of_words.each do |b|
+            sorted_word = b.upcase.chars.sort
+            if(sorted_word == original_word)
+                matches.push(b);
+            end
+        end
+
+        matches
+    end
+end
